a88ed8c751498204d4fc0265a4cfeb21defbf648
Denys Sttrilchuk <denia876@gmail.com>
2024-07-30 19:34:42 +0300

    fixed auth-email logic

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './common/pipes/validation.pipe';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Додайте налаштування CORS
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
 void bootstrap();
